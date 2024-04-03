07656d6ac83df59ed766a87c4d7fc43753989592
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-04-03 13:36:10 +0100

    Fix the path in the tailwind config file and move index.html to root directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./docs/**/*.{html,js}"],
+  content: ["./*.{html,js}", "./src/**/*.css"],
   theme: {
     extend: {
       fontFamily: {
