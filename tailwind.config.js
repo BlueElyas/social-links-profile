c30f0c60b5555aa03e93692e7d6aa976f0f90a4e
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-04-03 12:01:50 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primaryGreen: "hsl(75, 94%, 57%);",
+        neutralWhite: "hsl(0, 0%, 100%)",
+        neutralDarkGray: "hsl(0, 0%, 12%)",
+        neutralOffBlack: "hsl(0, 0%, 8%)",
+        neutralGray: "hsl(0, 0%, 20%)",
+      },
+    },
+    screens: {
+      sm: "500px",
+    },
+  },
+  plugins: [],
+};
